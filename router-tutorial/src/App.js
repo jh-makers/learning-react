54d3dd748916a722809276c3877c377d8859e9ae
jh-makers <dys04009@naver.com>
2023-03-16 21:06:08 +0900

    연습

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles/:id" element={<Article />} />
+    </Routes>
+  );
+};
+
+export default App;

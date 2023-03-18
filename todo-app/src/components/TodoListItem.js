47465d7382c8b542b2500bc7ec2c7dd680f8ac41
jh-makers <dys04009@naver.com>
2023-03-18 21:59:23 +0900

    간단한 메모 기능 todo-app

---
+++ todo-app/src/components/TodoListItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss';
+
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
+  const { id, text, checked } = todo;
+  return (
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
+      </div>
+    </div>
+  );
+};
+//TodoListItem 컴포넌트는 이제 todo, onRemove, onToggle 하지 않으면 리렌더링 되지 않는다.
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
